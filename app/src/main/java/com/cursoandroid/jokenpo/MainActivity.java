638676fffc0c558d01b446152296033810022684
Kaic Carvalho <kaic15_fernandes@hotmail.com>
2021-01-02 14:25:37 -0300

    Escolha de opção do usuário.

--- app/src/main/java/com/cursoandroid/jokenpo/MainActivity.java
+++ app/src/main/java/com/cursoandroid/jokenpo/MainActivity.java
@@ -3,6 +3,7 @@ package com.cursoandroid.jokenpo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void selecionadoPedra(View view){
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionadoPapel(View view){
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionadoTesoura(View view){
+        this.opcaoSelecionada("tesoura");
+    }
+
+    public void opcaoSelecionada(String opcaoSelecionada){
+
+
+
+    }
+
+
 }
